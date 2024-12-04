a956d65117dedb1c9a642b22813350fe9f7baa43
Thavrith <164502005+Thavrith-99@users.noreply.github.com>
2024-12-05 02:31:28 +0700

    Finish TP3

---
+++ src/Product_Store.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+export const useProductStore = defineStore('product', {
+    state: () => ({
+         groups: [],
+         promotions: [],
+         categories: [],
+         products: []
+    }),
+    getters: {},
+    actions: {
+        async fetchCategories() { 
+            await axios.get('http://localhost:3000/api/categories')
+            .then(response => {
+              // Access the data
+              // console.log(response.data); 
+              this.categories = response.data;
+            })
+            .catch(error => {
+              console.error('Error fetching data:', error);
+        });
+               },
+        async fetchPromotions() {
+            await axios.get('http://localhost:3000/api/promotions')
+            .then(response => {
+              // Access the data
+              // console.log(response.data); 
+              this.promotions = response.data;
+            })
+            .catch(error => {
+              console.error('Error fetching data:', error);
+        });
+                
+               }
+    },
+  })    
