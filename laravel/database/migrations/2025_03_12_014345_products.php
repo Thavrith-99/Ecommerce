891ffa3ecf505777daa48c2e71b5fab7d16fbf7f
Thavrith <164502005+Thavrith-99@users.noreply.github.com>
2025-03-12 23:36:54 +0700

    Finish Step1,2

--- laravel/database/migrations/2025_03_12_014345_products.php
+++ laravel/database/migrations/2025_03_12_014345_products.php
@@ -11,18 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function(Blueprint $table){
-            $table->id();
-            $table->string('name');
-            $table->bigInteger('category_id')->unsigned();
-            $table->double('pricing');
-            $table->text('description')->nullable();
-            $table->jsonb('image')->nullable();
-            $table->timestamps();
-            
-            $table->foreign('category_id')->references('id')->on('categories');
-
-        });
+        
         //
         //
     }
