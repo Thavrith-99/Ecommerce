d97896cd2e1c2bbaf76edb92a3e4759b15cc1c72
Thavrith <164502005+Thavrith-99@users.noreply.github.com>
2025-03-13 22:01:45 +0700

    Finish code

--- laravel/app/Http/Controllers/CategoryController.php
+++ laravel/app/Http/Controllers/CategoryController.php
@@ -2,32 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    // --- Get /api/categories
-    public function getCategories() {
-        return ["message" => "Getting list of categories"];
+
+    // Get /api/categories
+    public function getCategories(){
+      return response()->json(Category::all());
+        // return ['message' => "Getting list of categories"];
     }
 
-    // --- Post /api/categories
-    public function createCategory() {
-        return ["message" => "Creating 1 new category"];
+    // Post /api/categories
+    public function createCategory(Request $request){
+
+      $request->validate([
+          'name' => 'required|string|unique:categories',
+      ]);
+
+      $category = Category::create([
+        'name' => $request-> name
+      ]);
+
+      return response()->json([
+        'message'=>'Added successfully ! ',
+        'category'=> $category
+      ], 201);
+        // return ['message' => "Creating new 1 of category"];
+
     }
 
-    // --- Get /api/categories/{categoryId}
-    public function getCategory($categoryId) {
-        return ["message" => "Getting 1 category base on given categoryId"];
+      // Get /api/categories/{categoryId}
+      public function getCategory($categoryId){
+        $category = Category::findOrFail($categoryId);
+        if (!$category){
+          return response()->json([
+            'message'=> 'Category not found !'
+          ]);
+        }
+        return response()->json($category);
+
+
+        // return ['message' => "Getting 1 category by Id"];
+
     }
 
-    // --- Patch /api/categories/{categoryId}
-    public function updateCategory($categoryId) {
-        return ["message" => "Updating 1 category base on given categoryId"];
+      // Patch /api/categories/{categoryId}
+      public function updateCategory(Request $request, $categoryId){
+
+        $category = Category::findOrFail($categoryId);
+
+        $request->validate([
+          'name' => 'required|string|unique:categories,name,' . $categoryId
+      ]);
+
+        $category->update([ 'name'=> $request->name ]);
+
+        return response()->json([
+          'message'=>'updated successfully',
+          'category'=>$category
+        ]);
     }
 
-    // --- Delete /api/categories/{categoryId}
-    public function deleteCategory($categoryId) {
-        return ["message" => "Deleting 1 category base on given categoryId"];
+        // return ['message' => "Updating 1 of category by Id"];
+
+
+      // Delete /api/categories/{categoryId}
+      public function deleteCategory($categoryId){
+
+        $category = Category::findOrFail($categoryId);
+        $category->delete();
+
+        return response()->json(['message'=>'Deleted successfully !']);
+
+        // return ['message' => "Deleting 1 of category by Id"];
+
     }
+
+
 }
