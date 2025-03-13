374d15ba1c5478cf87a6a54626f7dd0d234d53b0
Thavrith <164502005+Thavrith-99@users.noreply.github.com>
2025-03-13 15:20:34 +0700

    Finish

--- laravel/database/migrations/2025_03_12_173950_create_personal_access_tokens_table.php
+++ laravel/database/migrations/2025_03_12_173950_create_personal_access_tokens_table.php
@@ -11,20 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function(Blueprint $table){
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
+            $table->morphs('tokenable');
             $table->string('name');
-            $table->bigInteger('category_id')->unsigned();
-            $table->double('pricing');
-            $table->text('description')->nullable();
-            $table->jsonb('image')->nullable();
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
-            
-            $table->foreign('category_id')->references('id')->on('categories');
-
         });
-        //
-        //
     }
 
     /**
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('personal_access_tokens');
     }
 };
